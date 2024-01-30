f83849db3eaf42ba72c119d5f782249f8c7ea99c
Kenwanjohi <wanjohiken85@gmail.com>
2024-01-30 23:49:47 +0300

    Fix script

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 protoc --plugin=$(pnpm root)/.bin/protoc-gen-ts_proto \
  --ts_proto_out=dist \
  --ts_proto_opt=outputServices=grpc-js \
+ --ts_proto_opt=esModuleInterop=true \
+ --ts_proto_opt=importSuffix=.js \
  -I=src/ src/**/*.proto
